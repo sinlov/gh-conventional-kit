8375fa304b92e0da4221cc26429b6de57072c4aa
sinlov <sinlovgmppt@gmail.com>
2023-07-26 10:37:14 +0800

    refactor: show more run info and move to github.com/sinlov/gh-conventional-kit/cmd/cli

--- cmd/gh-conventional-kit/main.go
+++ cmd/gh-conventional-kit/main.go
@@ -6,55 +6,14 @@ import (
 	"fmt"
 	"github.com/gookit/color"
 	"github.com/sinlov/gh-conventional-kit"
-	"github.com/sinlov/gh-conventional-kit/command"
-	"github.com/sinlov/gh-conventional-kit/command/subcommand_badge"
-	"github.com/sinlov/gh-conventional-kit/command/subcommand_markdown"
-	"github.com/sinlov/gh-conventional-kit/command/subcommand_template"
-	"github.com/sinlov/gh-conventional-kit/constant"
+	"github.com/sinlov/gh-conventional-kit/cmd/cli"
 	"github.com/sinlov/gh-conventional-kit/utils/pkgJson"
-	"github.com/sinlov/gh-conventional-kit/utils/urfave_cli"
-	"github.com/urfave/cli/v2"
 	os "os"
-	"runtime"
-	"time"
 )
 
 func main() {
 	pkgJson.InitPkgJsonContent(gh_conventional_kit.PackageJson)
-	app := cli.NewApp()
-	app.EnableBashCompletion = true
-	app.Version = pkgJson.GetPackageJsonVersionGoStyle(false)
-	app.Name = pkgJson.GetPackageJsonName()
-	if pkgJson.GetPackageJsonHomepage() != "" {
-		app.Usage = fmt.Sprintf("see: %s", pkgJson.GetPackageJsonHomepage())
-	}
-	app.Description = pkgJson.GetPackageJsonDescription()
-
-	year := time.Now().Year()
-	jsonAuthor := pkgJson.GetPackageJsonAuthor()
-	app.Copyright = fmt.Sprintf("© %s-%d %s by verson: %s",
-		constant.CopyrightStartYear, year, jsonAuthor.Name, runtime.Version())
-	author := &cli.Author{
-		Name:  jsonAuthor.Name,
-		Email: jsonAuthor.Email,
-	}
-	app.Authors = []*cli.Author{
-		author,
-	}
-
-	flags := urfave_cli.UrfaveCliAppendCliFlag(command.GlobalFlag(), command.HideGlobalFlag())
-
-	app.Flags = flags
-	app.Before = command.GlobalBeforeAction
-	app.Action = command.GlobalAction
-	app.After = command.GlobalAfterAction
-
-	var appCommands []*cli.Command
-	appCommands = urfave_cli.UrfaveCliAppendCliCommand(appCommands, subcommand_badge.Command())
-	appCommands = urfave_cli.UrfaveCliAppendCliCommand(appCommands, subcommand_markdown.Command())
-	appCommands = urfave_cli.UrfaveCliAppendCliCommand(appCommands, subcommand_template.Command())
-
-	app.Commands = appCommands
+	app := cli.NewCliApp()
 
 	args := os.Args
 	if len(args) < 2 {
